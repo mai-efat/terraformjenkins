3379c6036733e18917abc52a09ede3374ab10872
maieffat <maieffat419@gmail.com>
2024-11-16 22:16:43 +0200

    add

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,28 @@
+
+resource "aws_instance" "ec2-private-1" {
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = "t3.micro"
+  key_name                    = "key1"
+ 
+
+  network_interface {
+    network_interface_id = aws_network_interface.my_network_interface.id
+    device_index         = 0
+  }
+
+ 
+  lifecycle {
+    ignore_changes = [
+      vpc_security_group_ids,
+      root_block_device[0].volume_size,     # or other attributes in root_block_device
+      root_block_device[0].delete_on_termination
+    ]
+  }
+  
+  tags = {
+    Name = "apache-1"
+  }
+}
 resource "aws_instance" "ec2-public-1" {
   ami                         = data.aws_ami.amazon_linux_2.id
   instance_type               = "t3.micro"
